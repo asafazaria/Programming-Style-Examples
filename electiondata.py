3c068b2470acf9c60a479134e4a8af6bc2d4ff08
Asaf Azaria <azaria@mit.edu>
2014-12-13 15:35:50 -0500

    Adding votes count to elections data

--- electiondata.py
+++ electiondata.py
@@ -14,5 +14,14 @@ class ElectionResults:
             all_names.append(columns[1])
         return all_names[1:]
 
+    def votes_count(self):
+        total_votes = {'Obama': 0,
+                       'Romney': 0}
+        test = sum([int(line.split(',')[3]) for line in self.all_lines[1:]])
+        print test
+        total_votes['Obama'] = sum([int(line.split(',')[3]) for line in self.all_lines[1:]])
+        total_votes['Romney'] = sum([int(line.split(',')[5]) for line in self.all_lines[1:]])
+        return total_votes
+
     def state_count(self):
         return len(self.states())
