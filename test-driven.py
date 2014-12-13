00bb72e6981fbaf183d0f833188dc48f6ade5c93
asafazaria <azaria@mit.edu>
2014-12-13 15:55:48 -0500

    Adding unit test for votes count

--- test-driven.py
+++ test-driven.py
@@ -25,6 +25,16 @@ class ElectionResultsTest(unittest.TestCase):
         assert len(names)==2
         assert names[0]=='Alaska'
         assert names[1]=='Alabama'
+    
+    def testVotesCount(self):
+        self.results.load()
+        votes_count = self.results.votes_count()
+        print votes_count
+        expected_obama = 885316
+        expected_romney = 1373687
+        assert votes_count['Obama'] == expected_obama, "count for Obama is %s rather than %s" % (votes_count['Obama'], expected_obama) 
+        assert votes_count['Romney'] == expected_romney, "count for Romney is %s rather than %s" % (votes_count['Romney'], expected_romney)
+        
 
 
 # if this file is run directly, run the tests
